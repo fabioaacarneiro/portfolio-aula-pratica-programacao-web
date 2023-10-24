3daed448b4495556682590f38a46c4f4e2c815a9
Fabio Carneiro <fabio@iMac-Pro-de-Fabio.local>
2023-10-24 19:31:58 -0300

    criado a camada de resources e os endpoints do crud do usur

--- src/main/java/resource/UserResource.java
+++ src/main/java/resource/UserResource.java
@@ -1,14 +1,15 @@
 package resource;
 
+import java.net.URI;
 import java.util.List;
 
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import entities.User;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import services.UserService;
 
 @RestController
@@ -22,5 +23,34 @@ public class UserResource {
 	public ResponseEntity<List<User>> findAll(){
 		List<User> users = userService.findAll();
 		return ResponseEntity.ok().body(users);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user);
+	}
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user) {
+		user = userService.insert(user);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(user.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(user);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<User> insert(@PathVariable Long id, @RequestBody User user) {
+		user = userService.update(id, user);
+		return ResponseEntity.ok().body(user);
 	}
 }
